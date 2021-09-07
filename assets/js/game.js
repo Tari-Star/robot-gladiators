a69598875d740c8f6ed48e7912b6e652696edd4e
Tari <bota.maral@gmail.com>
2021-09-07 08:21:22 -0400

    fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,26 +41,25 @@ var fightOrSkip = function() {
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   // keep track of who goes first
+  var isPlayerTurn = true;
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
 
-
-
-  while (playerInfo.health > 0 && enemy.health > 0) {
-
-
-    if (fightOrSkip()) {
-      // if true, leave fight by breaking loop
-      break;
-    }
-       
-        
+   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
           
         enemy.health = Math.max(0, enemy.health - damage);
-
         console.log(
         playerInfo.name + " attacked " + enemy.name + " . " + enemy.name + " now has " + enemy.health + " health remaining."
         );
-
          // check enemy's health
         if ( enemy.health <= 0){
           window.alert( enemy.name + " has died!");
@@ -89,7 +88,9 @@ var fight = function(enemy) {
         }  else {
           window.alert( playerInfo.name + " still has " + playerInfo.health + "health left.");
         }
-
+      }
+      // switch turn order for next round
+      isPlayerTurn = !isPlayerTurn;
     }   
 };
 // function to start a new game
