99d83cc66cb692b8ff6d3cb500f4f1273816cedc
Tari <bota.maral@gmail.com>
2021-09-07 07:58:52 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,7 +26,7 @@ var fightOrSkip = function() {
     
     // if yes (true), leave fight
     if (confirmSkip) {
-      window.alert(playerInfo.name + "has decided to skip this fight. Goodbye!");
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
       //substract money from playerMoney for skipping
       playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
       // stop while() loop using break; and enter next fight   
@@ -155,28 +155,22 @@ var startGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
    // use switch to carry out action
+  shopOptionPrompt = parseInt(shopOptionPrompt);
    switch (shopOptionPrompt) {
-     case "REFILL":
-     case "refill":
+     case 1:
       playerInfo.refillHealth();
         break;
-      case "UPGRADE":
-      case "upgrade":
+      case 2:
        playerInfo.upgradeAttack();
         break;
-      case "LEAVE":
-      case "leave":
+      case 3:
           window.alert("Leaving the store.");
-
-          // do nothing, so function will end
           break;
           default:
             window.alert("You did not pick a valid option. Try again.");
-
-            //call shop() again to force player to pick a valid option
             shop();
             break;
    }
